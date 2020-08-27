926dbc874fb16c902c1b569ba0523225e7723f59
Nogmog <iexplodeh@hotmail.com>
2020-08-27 02:01:52 +0100

    Adding basic operation algorithm

---
+++ main.js
@@ -0,0 +1,14 @@
+const selAll = document.querySelector("#container");
+
+const selScreen = selAll.querySelector("#screen");
+const selOper = selAll.querySelector("#operation");
+
+
+const add = (a, b) => {return a + b}
+const sub = (a, b) => {return a - b}
+const multi = (a, b) => {return a * b}
+
+function divide(a, b){
+    return a / b;
+}
+
