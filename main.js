aa691da00a3f4fab436a714429539b84ba514637
Nogmog <iexplodeh@hotmail.com>
2020-08-30 01:19:03 +0100

    Added logic to operator buttons and shows on screen

--- main.js
+++ main.js
@@ -20,6 +20,22 @@ function divide(a, b){
     return a / b;
 }
 
+function doCalc(operator, numA, numB){
+    if(operator = "add"){
+        return add(numA, numB);
+    }else if(operator = "subtract"){
+        return sub(numA, numB);
+    }else if(operator = "multiply"){
+        return multi(numA, numB);
+    }else if(operator = "divide"){
+        return divide(numA, numB);
+    }
+    else{console.log("Error")}
+}
+
+
+let prevOperate = ""
+
 numBtn.forEach(e => e.addEventListener("mouseup", showValue));
 function showValue(e){
     const numValue = e.target.getAttribute("value");
@@ -29,14 +45,23 @@ function showValue(e){
 
 allOperateBtn.forEach(e => e.addEventListener("mouseup", calculate));
 function calculate(e){
-    if (ansTxt.textContent != 0){
+    let ans = "error"
+    if (prevOperate != ""){ // past operation
+        console.log("2nd number");
         const operator = e.target.id;
-        console.log(operator)
-    }else{}
-    console.log(e)
+        ans = doCalc(operator, Number(prevTxt.textContent.slice(0 ,-2)), Number(ansTxt.textContent));
+    }else{ //no operation
+        ans = ansTxt.textContent;
+    }
+    console.log(ans)
+    const sign = e.target.getAttribute("value");
+    prevTxt.textContent = ans + " " + sign;
+    ansTxt.textContent = "0";
+    prevOperate = e.target.id;
 }
 
 clearBtn.addEventListener("mouseup", function(){
     prevTxt.textContent = ""
     ansTxt.textContent = "0"
+    prevOperate = ""
 });
